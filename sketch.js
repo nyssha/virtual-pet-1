02bea7a5c0140559091b7569b780be6703ed4f52
nyssha <65846349+nyssha@users.noreply.github.com>
2020-08-08 12:03:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var dog
+var dog1
+
+function preload()
+{
+  dog.createSprite(400,400,20,20)
+  dog.loadImage(images/dogImg.png)
+
+  dog1.createSprite(400,200,20,20)
+  dog1.loadImage(images/dogImg1.png)
+}
+//function setup() {
+	//createCanvas(800, 800);
+  
+//}
+
+
+//function draw() {  
+
+  drawSprites();
+  //add styles here
+
+//}
+
+
+
